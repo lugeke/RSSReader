2f63e16025c5b94f3e2e1f6e41659eb9a65da934
lugeke <lugeke@gmail.com>
2014-10-19 11:32:38 +0800

    add zoom-out page transformer

--- app/src/main/java/com/example/lugeke/rssreader/EntryListActivity.java
+++ app/src/main/java/com/example/lugeke/rssreader/EntryListActivity.java
@@ -82,14 +82,43 @@ public class EntryListActivity extends FragmentActivity  {
 
         mPagerAdapter=new EntryListPagerAdapter(getSupportFragmentManager());
 
-
         mViewPager=(ViewPager)findViewById(R.id.pager);
         mViewPager.setAdapter(mPagerAdapter);
-
+        mViewPager.setPageTransformer(true,new ZoomOutPageTransformer());
 
     }
 
 
+    public static class ZoomOutPageTransformer implements ViewPager.PageTransformer{
+
+        private static final float MIN_SCALE=0.85F;
+        private static final float MIN_ALPHA=0.5F;
+        @Override
+        public void transformPage(View view, float position) {
+            int pageWidth =view.getWidth();
+            int pageHeight=view.getHeight();
+            if(position<-1){
+                view.setAlpha(0);
+            }else if(position<=1){
+                float scaleFactor= Math.max(MIN_SCALE,1- Math.abs(position));
+                float vertMargin=pageHeight*(1-scaleFactor)/2;
+                float horzMargin = pageWidth*(1-scaleFactor)/2;
+                if(position<0){
+                    view.setTranslationX(horzMargin-vertMargin/2);
+
+                }else{
+                    view.setTranslationX(-horzMargin+vertMargin/2);
+                }
+
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                view.setAlpha(MIN_ALPHA+(scaleFactor-MIN_SCALE)/(1-MIN_SCALE)*(1-MIN_ALPHA));
+            }else{
+                view.setAlpha(0);
+            }
+        }
+    }
 
 
     public static class EntryListPagerAdapter extends FragmentPagerAdapter {
